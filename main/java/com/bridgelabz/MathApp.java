f4136c927d90351fca1754615782346350b3d0e1
Shivam Mole <shivammole1713@gmail.com>
2024-10-04 14:59:58 +0530

    passing lambda funciotn to show result

--- main/java/com/bridgelabz/MathApp.java
+++ main/java/com/bridgelabz/MathApp.java
@@ -10,11 +10,13 @@ public class MathApp {
             if (num2 == 0) {
                 throw new ArithmeticException("cannot devide by zero");
             }
-            ;
             return num1 / num2;
         };
         MathOperation mul=(num1,num2)->num1*num2;
 
         MathOperation.print_result(1,2,"additoin",addition);
+        MathOperation.print_result(1,4,"division",div);
+        MathOperation.print_result(2,2,"multilication",mul);
+        MathOperation.print_result(11,21,"Substractoin ",subs);
     }
 }
