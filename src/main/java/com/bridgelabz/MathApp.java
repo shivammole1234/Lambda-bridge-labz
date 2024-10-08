e30cd66da8820888d22f29410191ec4aa1cded22
Shivam Mole <shivammole1713@gmail.com>
2024-10-08 13:40:31 +0530

    Ability to Iterate through List of number and print only if it is even - Use predicate Functional Interface to check

--- src/main/java/com/bridgelabz/MathApp.java
+++ src/main/java/com/bridgelabz/MathApp.java
@@ -1,9 +1,11 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 class Num_list_consumer implements Consumer<Integer>{
     public void accept(Integer num){
@@ -30,7 +32,7 @@ public class MathApp {
 //        MathOperation.print_result(11,21,"Substractoin ",subs);
 //
 
-        List<Integer> numbers= Arrays.asList(1,2,4,6,7,8,9);
+        List<Integer> numbers= Arrays.asList(1,2,4,6,7,8,9,11,10,12,14,13,19,18);
         System.out.println("This method is using the proper class which is Consumer interface");
         numbers.forEach(new Num_list_consumer());
 
@@ -54,5 +56,20 @@ public class MathApp {
         System.out.println("using the custon fucntional interface");
         numbers.forEach(num-> System.out.println("Original num :- "+num+" DOuble num:- " + int_convert_double.convert(num)));
 
+        // even number using predicate with help of forEach loop
+
+        Predicate<Integer> is_even=num->num%2==0;
+        List<Integer> even_list= new ArrayList<>();
+        System.out.println("Even number in list ");
+        numbers.forEach(num->{
+            if(is_even.test(num)){
+                System.out.println(num);
+                even_list.add(num);
+            }
+        });
+        System.out.println("even list number are \n"+even_list);
+        
     }
+
+
 }
